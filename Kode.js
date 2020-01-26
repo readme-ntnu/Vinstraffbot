dd64dbf14044f8856063da79e6a5f233a51145f3
Andreas Hammer Håversen <37153174+AndreasHaaversen@users.noreply.github.com>
2020-01-26 18:05:42 +0100

    off-by-one error

--- Kode.js
+++ Kode.js
@@ -1,9 +1,10 @@
+// NOTE: 0-indexed
 var NAME_COL = 6;
 var NUM_COL = 7;
 var MUST_BRING_COL = 8;
 var START_ROW = 5;
-var END_ROW = 28;
-var TOTAL_ROW = 29;
+var END_ROW = 29;
+var TOTAL_ROW = 30;
 
 var Actions = {
   hent: {
